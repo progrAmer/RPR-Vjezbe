4731ef976b360202ab60d602e8853b3657381cac
Amer Muratagic <muratagic.amer@gmail.com>
2023-10-24 16:31:44 +0200

    LV2-Z3

--- rpr-t2-z3/src/main/java/ba/unsa/etf/rpr/Main.java
+++ rpr-t2-z3/src/main/java/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,36 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    List<Integer> numbers = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.print("Enter a number: ");
+      String value = scanner.nextLine();
+
+      if (isNumber(value)) {
+        numbers.add(Integer.parseInt(value));
+      } else if (value.equals("stop")) {
+        break;
+      }
+
+      System.out.println(numbers);
+    }
+  }
+
+  public static boolean isNumber(String x) {
+    try {
+      Integer.parseInt(x);
+      return true;
+    } catch (NumberFormatException ex) {
+      return false;
+    }
   }
 }
